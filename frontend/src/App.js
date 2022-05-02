9f635aa5e0ea90e5ec7b58b2a5f201867e48213a
karan221997 <karan.jariwala221997@gmail.com>
2022-05-01 17:53:16 -0700

    Frntend Backend UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-//import Login from './components/Login/Login';
-import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+//import Signup from './components/Signup/Signup';
 
 
 function App() {
-  //return <Login/>
-  return <Signup/>
+  return <Login/>
+  //return <Signup/>
 }
 
 export default App;
