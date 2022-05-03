6bb9dd19af4e7d683c85135c2df87782290409b5
karan221997 <karan.jariwala221997@gmail.com>
2022-05-03 11:18:32 -0700

    allMerge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Router, Route, Routes, Switch, BrowserRouter } from "react-router-dom";
 import QA from "../src/components/QA/QA";
 import Dashboard from "../src/components/Dashboard/Dashboard";
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
           <Route exact path="/qa" element={<QA />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
