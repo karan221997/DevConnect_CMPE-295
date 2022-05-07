f1a2d40b701ec2f519f1021e40bbdd472a759f19
Yusuf Soni <yusufjsoni@gmail.com>
2022-05-07 12:48:03 -0700

    Added create question modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,43 @@
-import { Router, Route, Routes, Switch, BrowserRouter, Navigate } from "react-router-dom";
+import {
+  Router,
+  Route,
+  Routes,
+  Switch,
+  BrowserRouter,
+  Navigate,
+} from "react-router-dom";
 import Dashboard from "./Pages/dashboard/Dashboard";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 import Messenger from "./Pages/messenger/Messenger";
+import CreatePost from "./components/CreatePost/CreatePost";
 
 function App() {
   const { user } = useContext(AuthContext);
   return (
     <div className="App">
-
       <BrowserRouter>
         <Routes>
-          <Route exact path="/login" element={user ? <Dashboard /> : <Login />} />
-          <Route exact path="/signup" element={user ? <Navigate to="/login" /> : <Signup />} />
-          <Route exact path="/dashboard" element={user ? <Dashboard /> : <Login />} />
-          <Route exact path="/messenger" element={ <Messenger />} />
+          <Route
+            exact
+            path="/login"
+            element={user ? <Dashboard /> : <Login />}
+          />
+          <Route
+            exact
+            path="/signup"
+            element={user ? <Navigate to="/login" /> : <Signup />}
+          />
+          <Route
+            exact
+            path="/dashboard"
+            element={user ? <Dashboard /> : <Login />}
+          />
+          <Route exact path="/messenger" element={<Messenger />} />
+          {/* <Route exact path="/createPost" element={<CreatePost />} />
+          <Route exact path="/Dashboard" element={<Dashboard />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
