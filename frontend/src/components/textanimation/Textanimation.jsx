11e77ed0c9d844a026ed7ea897db6a153a77a20d
harshalad0302 <harshalad03021996@gmail.com>
2022-05-06 21:30:23 -0700

    Landing page

---
+++ frontend/src/components/textanimation/Textanimation.jsx
@@ -0,0 +1,23 @@
+import './textanimation.css'
+
+export default function Textanimation() {
+    return (
+    <div className="mainclasscontainer">
+    
+ <div class="content">
+     <div class="content__container">
+                <p class="content__container__text">
+                Every
+                </p>
+                <ul class="content__container__list">
+                <li class="content__container__list__item">Developer</li>
+                <li class="content__container__list__item">System Admin</li>
+                <li class="content__container__list__item"> Mobile Developer</li>
+                <li class="content__container__list__item"> Data Scientist</li>
+                </ul>
+          </div>
+          <span className="textafter"> Has a tab open to Dev Connect</span>
+</div>
+</div>     
+    );
+}
