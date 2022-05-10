bebaf448a08a231ddbdab7bf7acd8a3f06ccf626
DaneshDhamejani <daneshdhamejani24@gmail.com>
2022-05-09 18:57:59 -0700

    Adding create and fetch post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -8,9 +8,10 @@ router.post("/addPost", async (req, res) => {
         console.log(req.body);
         const post = await new Post({
             userId: req.body.userId,
-            email:req.body.email,
-            name:req.body.name,
-            text:req.body.text,
+            postCreatorEmail:req.body.email,
+            postCreatorUserName:req.body.userName,
+            postTitle:req.body.title,
+            postText:req.body.text,
             image:req.body.image,
             upVotes:req.body.upVotes,
             downVotes:req.body.downVotes,
@@ -27,17 +28,21 @@ router.post("/addPost", async (req, res) => {
 });
 
 //all the post that will be on users dashboard - only the ones that he is part of that community
-router.get("/getAllPost", async (req, res) => {
+router.post("/getAllPost", async (req, res) => {
     try{
         console.log("Inside get post");
         console.log(req.body.email);
         //we will get following from frnt end 
-        const userDetails = await User.find({email:req.body.email});
-        console.log(userDetails.following[0]);
+        // const userDetails = await User.find({email:req.body.email});
+        // console.log(userDetails.following[0]);
 
-        //working properly if user details is array [1,2,3]
-        const allPosts = await Post.find().where('communityId').in(userDetails).exec();
+        // //working properly if user details is array [1,2,3]
+        // const allPosts = await Post.find().where('communityId').in(userDetails).exec();
+        // res.status(200).json(allPosts);
+
+        const allPosts=await Post.find()
         res.status(200).json(allPosts);
+        
     }
     catch(err){
         res.status(500).json({message: err});
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -2,16 +2,14 @@
 import Share from '../share/Share';
 import Post from '../post/Post';
 import './feed.css';
-import {Posts} from "../../dummyData.js";
+//import {Posts} from "../../dummyData.js";
 
 export default function Feed() {
   return (
     <div className="feed">
         <div className="feedWrapper">
-            <Share />
-            {Posts.map((p) => (
-             <Post key={p.id} post={p}/>
-            ))}     
+            {/* <Share /> */}
+            <Post />     
         </div>
     </div>
   );
