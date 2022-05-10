1972253fecf127bebceef3063e3a666583b93a4f
harshalad0302 <harshalad03021996@gmail.com>
2022-05-10 00:31:17 -0700

    post display chnages

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -30,23 +30,13 @@ router.post("/addPost", async (req, res) => {
 //all the post that will be on users dashboard - only the ones that he is part of that community
 router.post("/getAllPost", async (req, res) => {
     try{
-        console.log("Inside get post");
-        console.log(req.body.email);
-        //we will get following from frnt end 
-        // const userDetails = await User.find({email:req.body.email});
-        // console.log(userDetails.following[0]);
-
-        // //working properly if user details is array [1,2,3]
-        // const allPosts = await Post.find().where('communityId').in(userDetails).exec();
-        // res.status(200).json(allPosts);
-
-        const allPosts=await Post.find()
-        res.status(200).json(allPosts);
-        
+        console.log("Inside get all post");
+        console.log(req.body);
+        const posts = await Post.find();
+        res.status(200).json({posts});
     }
     catch(err){
         res.status(500).json({message: err});
-    
     }
 });
 
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -2,14 +2,33 @@
 import Share from '../share/Share';
 import Post from '../post/Post';
 import './feed.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 //import {Posts} from "../../dummyData.js";
 
 export default function Feed() {
+
+  //get all the posts and pass it as props
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+      async function fetchPosts() {
+        const user = JSON.parse(localStorage.getItem('user'));
+       console.log("email is: " + JSON.stringify(user.email));
+       const result =  await axios.post("api/posts/getAllPost",JSON.stringify(user.email))
+       console.log("posts are",result.data.posts);
+       setPosts(result.data.posts);
+    }
+    fetchPosts();
+  }, []);
+
+
   return (
     <div className="feed">
         <div className="feedWrapper">
-            {/* <Share /> */}
-            <Post />     
+           {posts.map((p) => (
+             <Post key={p.id} post={p}/>
+            ))}  
         </div>
     </div>
   );
